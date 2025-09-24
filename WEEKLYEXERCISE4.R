7fdb3d7d7f2fb4a7b55b3bb7bb508f6bab15d4ea
Yukinoshita <hyang725@uic.edu>
2025-09-24 02:44:31 -0500

    added answer for Question 7.a

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -2,3 +2,11 @@
 install.packages("usethis")
 library(usethis)
 system("git branch --show-current")
+
+# Q 7
+
+# a
+delegation_1992 <- olympics %>%
+  filter(year == 1992) %>%
+  select(country, athletes) %>%
+  arrange(desc(athletes))
