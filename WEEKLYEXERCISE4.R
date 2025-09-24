6f970db48f74ddbc04bf417b6c4c2d9d37d80b2b
Yukinoshita <hyang725@uic.edu>
2025-09-24 02:45:27 -0500

    added answer for question 7.b

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -10,3 +10,16 @@ delegation_1992 <- olympics %>%
   filter(year == 1992) %>%
   select(country, athletes) %>%
   arrange(desc(athletes))
+
+# b
+selected_countries <- c("United States", "France", "Germany", "Russia", "China")
+
+gold_plot <- olympics %>%
+  filter(country %in% selected_countries) %>%
+  group_by(year, country) %>%
+  summarise(total_gold = sum(gold, na.rm = TRUE)) %>%
+  ggplot(aes(x = year, y = total_gold, color = country)) +
+  geom_line(size = 1.2) +
+  labs(title = "Gold Medals Over Time",
+       y = "Gold Medals",
+       x = "Year")
