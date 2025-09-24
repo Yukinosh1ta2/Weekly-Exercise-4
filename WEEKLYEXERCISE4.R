83e46584cee5c08626824163bddbe1a0cc446dc1
Yukinoshita <hyang725@uic.edu>
2025-09-24 02:31:42 -0500

    added answer for question 6.a

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -2,3 +2,13 @@
 install.packages("usethis")
 library(usethis)
 system("git branch --show-current")
+
+# Q 6
+library(readr)
+library(tidyverse)
+library(ggplot2)
+olympics <- read_csv("Olympics.csv")
+
+# a. 
+olympics <- olympics %>%
+  mutate(total.medals = gold + silver + bronze)
